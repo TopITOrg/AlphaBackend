76489f311430a4168e0b64c1eb8d5a613de02f8f
KirAPavlov <kirapavlov@mai.education>
2025-10-23 21:52:19 +0300

    add null group id handling, now returns group and group name

--- internal/sqlc/queries/users.sql
+++ internal/sqlc/queries/users.sql
@@ -42,20 +42,19 @@ WITH user_info AS (
     (full_name, social_network_link, phone_number, email, birth_date, role, password, group_id)
     VALUES (@full_name, @social_network_link, @phone_number, @email, @birth_date, @role, @password, @group_id)
     RETURNING *
-),
-group_info AS (
-    SELECT 
-        (groups.prefix || '-' || 
-         extract(YEAR FROM age(now(), (groups.enrollment_year::text || '-09-01 00:00:00')::timestamptz)) + 1 || 
-         lpad(groups.group_number::text, 2, '0') || 
-         group_types.name || '-' || 
-         substring(groups.enrollment_year::text FROM 3 FOR 2)
-        )::text as group_name
-    FROM groups
-    LEFT JOIN group_types ON groups.group_type_id = group_types.id AND group_types.is_deleted = false
-    WHERE groups.id = @group_id AND groups.is_deleted = false
 )
 SELECT 
     user_info.*,
-    group_info.group_name
-FROM user_info, group_info;
+    CASE 
+        WHEN groups.id IS NOT NULL THEN
+            (groups.prefix || '-' || 
+             extract(YEAR FROM age(now(), (groups.enrollment_year::text || '-09-01 00:00:00')::timestamptz)) + 1 || 
+             lpad(groups.group_number::text, 2, '0') || 
+             group_types.name || '-' || 
+             substring(groups.enrollment_year::text FROM 3 FOR 2)
+            )::text
+        ELSE ''
+    END as group_name
+FROM user_info
+LEFT JOIN groups ON groups.id = user_info.group_id AND groups.is_deleted = false
+LEFT JOIN group_types ON groups.group_type_id = group_types.id AND group_types.is_deleted = false;
